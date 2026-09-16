2f14536d1625da15a114408c3fe64b5eddaa7992
AstroEarthly <astroearthly@gmail.com>
2026-09-16 19:08:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: '/Portfolio_WIP/',
-  },
   tanstackStart: {
     server: { entry: "server" },
-    nitro: {
-      preset: 'static',
-    },
   },
 });
